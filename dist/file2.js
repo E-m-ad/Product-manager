e79c4388fabcf3bba5db28f9194ba7b28df3dd3c
Mohamed Emad <104107994+E-m-ad@users.noreply.github.com>
2022-11-12 21:13:39 +0200

    Add files via upload

---
+++ dist/file2.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("emad");
+//# sourceMappingURL=file2.js.map
---
+++ src/file.ts
@@ -0,0 +1,275 @@
+// Functions needed
+/**
+ * getData()
+ * totalData()
+ * count()
+ */
+//first we need to get all elements
+let nameOfProduct: any = document.getElementById("nameOfProduct");
+let priceChildern: any = document.querySelector(".price");
+let originalPrice: any = priceChildern?.children[0];
+let sellPrice: any = priceChildern?.children[1];
+let discount: any = priceChildern?.children[2];
+let spanPirce: any = document.querySelector(".totalprice");
+let total: any = priceChildern?.children[3];
+let count: any = document.getElementById("count");
+let gender: any = document.getElementById("Gender");
+let createButton: any = document.getElementById("CreateElement");
+let table: any = document.querySelector("table");
+let allInputs = document.querySelectorAll("input");
+let mood = "create";
+let tmp;
+let searchButton: any = document.getElementById("search");
+let searchIcon: any = document.getElementById("searchingIcon");
+let ulChildern = document.querySelectorAll("ul li");
+createButton.addEventListener("click", getDate);
+
+function getTotal() {
+  spanPirce.innerHTML =
+    +sellPrice.value - +originalPrice.value - +discount.value;
+}
+let storageArray: any = [];
+if (localStorage.product) {
+  storageArray = JSON.parse(localStorage.product);
+}
+function getDate() {
+  if (nameOfProduct.value !== "") {
+    allInputs.forEach(function (e) {
+      if (e.value !== "") {
+        let object = {
+          title: nameOfProduct.value,
+          originalPrice: originalPrice.value || "-",
+          sellPrice: sellPrice.value || "-",
+          count: count.value || 1,
+          gender: gender.value || "-",
+          discount: discount.value || "-",
+          spanPirce: spanPirce.innerHTML || "-",
+          totalGain: spanPirce.innerHTML * count.value || "-",
+        };
+        if (mood === "create") {
+          storageArray.push(object);
+        } else {
+          storageArray[tmp] = object;
+          count.style.display = "block";
+        }
+
+        localStorage.setItem("product", JSON.stringify(storageArray));
+        clearDate();
+        readDate();
+        deleteButton();
+      }
+    });
+  } else {
+    alert("من فضلك أدخل اسم المنتج");
+  }
+}
+deleteButton();
+function deleteButton() {
+  if (storageArray.length > 0) {
+    let button: any = document.getElementById("DeleteButton");
+    button.innerHTML = ` <button onclick ="deleteAll()">حذف الكل (${storageArray.length})  </button>`;
+  } else {
+    let button: any = document.getElementById("DeleteButton");
+    button.innerHTML = ``;
+  }
+}
+
+function clearDate() {
+  allInputs.forEach(function (e) {
+    e.value = "";
+  });
+  spanPirce.innerHTML = "";
+}
+
+function readDate() {
+  let table = "";
+  for (let i = 0; i < storageArray.length; i++) {
+    table += ` 
+    <tr>
+    <td>${i + 1}</td>
+    <td>${storageArray[i].title}</td>
+    <td>${storageArray[i].originalPrice}</td>
+    <td>${storageArray[i].sellPrice}</td>
+    <td>${storageArray[i].discount}</td>
+    <td>${storageArray[i].gender}</td>
+    <td>${storageArray[i].count}</td>
+    <td>${storageArray[i].spanPirce}</td>
+    <td>${storageArray[i].totalGain}</td>
+    <td><button onclick="deleteDate(${i})" >حذف</button></td>
+    <td><button onclick="updateDate(${i})">تعديل</button></td>
+    <td><button onclick="decrement(${i})">-</button></td>
+    </tr>
+    `;
+
+    let tbody: any = document.getElementById("tbody");
+    tbody.innerHTML = table;
+  }
+}
+function decrement(i) {
+  if (storageArray[i].count > 0) {
+    storageArray[i].count = +storageArray[i].count - 1;
+    localStorage.setItem("product", JSON.stringify(storageArray));
+    readDate();
+  } else {
+    let obj = {
+      name: storageArray[i].title,
+      number: ++i,
+    };
+    boxArray.push(obj);
+    localStorage.setItem("Needed", JSON.stringify(boxArray));
+    readingDate();
+  }
+}
+
+function deleteDate(i: any) {
+  if (storageArray.length === 0) {
+    let tbody: any = document.getElementById("tbody");
+    tbody.innerHTML = "";
+  } else {
+    storageArray.splice(i, 1);
+    localStorage.product = JSON.stringify(storageArray);
+    deleteButton();
+    if (searchButton.value === "") {
+      readDate();
+    }
+  }
+}
+function deleteAll() {
+  storageArray.splice(0);
+  localStorage.product = JSON.stringify(storageArray);
+  readDate();
+  let tbody: any = document.getElementById("tbody");
+  tbody.innerHTML = "";
+  let button: any = document.getElementById("DeleteButton");
+  button.innerHTML = ` `;
+}
+function updateDate(i) {
+  nameOfProduct.value = storageArray[i].title;
+  originalPrice.value = storageArray[i].originalPrice;
+  sellPrice.value = storageArray[i].sellPrice;
+  discount.value = storageArray[i].discount;
+  spanPirce.innerHTML = storageArray[i].spanPirce;
+  total.value = storageArray[i].totalGain;
+  gender.value = storageArray[i].gender;
+  createButton.innerHTML = "تعديل العنصر";
+  count.style.display = "none";
+  mood = "update";
+  tmp = i;
+}
+readDate();
+
+function search(value) {
+  let table = "";
+
+  for (let i = 0; i < storageArray.length; i++) {
+    if (storageArray[i].title.includes(value)) {
+      table += ` 
+      <tr>
+      <td>${i + 1}</td>
+      <td>${storageArray[i].title}</td>
+      <td>${storageArray[i].originalPrice}</td>
+      <td>${storageArray[i].sellPrice}</td>
+      <td>${storageArray[i].discount}</td>
+      <td>${storageArray[i].gender}</td>
+      <td>${storageArray[i].count}</td>
+      <td>${storageArray[i].spanPirce}</td>
+      <td>${storageArray[i].totalGain}</td>
+      <td><button onclick="deleteDate(${i})" >حذف</button></td>
+      <td><button onclick="updateDate(${i})">تعديل</button></td>
+      </tr>
+      `;
+    }
+  }
+  let tbody: any = document.getElementById("tbody");
+  tbody.innerHTML = table;
+}
+if (searchButton.value !== "") {
+  for (let i = 0; i < storageArray.length; i++) {
+    if (storageArray[i].title.includes(searchButton.value)) {
+      table += ` 
+      <tr>
+      <td>${i + 1}</td>
+      <td>${storageArray[i].title}</td>
+      <td>${storageArray[i].originalPrice}</td>
+      <td>${storageArray[i].sellPrice}</td>
+      <td>${storageArray[i].discount}</td>
+      <td>${storageArray[i].gender}</td>
+      <td>${storageArray[i].count}</td>
+      <td>${storageArray[i].spanPirce}</td>
+      <td>${storageArray[i].totalGain}</td>
+      <td><button onclick="deleteDate(${i})" >حذف</button></td>
+      <td><button onclick="updateDate(${i})">تعديل</button></td>
+    
+      </tr>
+      `;
+    }
+  }
+  let tbody: any = document.getElementById("tbody");
+  tbody.innerHTML = table;
+}
+ulChildern.forEach(function (e) {
+  for (let i = 0; i < ulChildern.length; i++) {
+    ulChildern[i].addEventListener("click", function () {
+      e.classList.remove("active");
+      ulChildern[i].classList.add("active");
+    });
+  }
+});
+
+//=============================================================================================
+//second one
+let nameOfNeededElements: any = document.getElementById("nameOfNeeded");
+let numberOfNeededElements: any = document.getElementById("numberOfNeeded");
+let buttonCreated: any = document.getElementById("neededproductsButtons");
+//first function to get date form inputs
+let boxArray: any[] = [];
+if (localStorage.Needed) {
+  boxArray = JSON.parse(localStorage.Needed);
+}
+buttonCreated.addEventListener("click", function () {
+  if (nameOfNeededElements.value !== "") {
+    let obj = {
+      name: nameOfNeededElements.value,
+      number: numberOfNeededElements.value,
+    };
+    boxArray.push(obj);
+    localStorage.setItem("Needed", JSON.stringify(boxArray));
+    readingDate();
+  } else {
+    alert("من فضلك ادخل اسم المنتج");
+  }
+});
+
+//second function we need is to read the data on the screen
+
+function readingDate() {
+  let table = "";
+  for (let i = 0; i < boxArray.length; i++) {
+    table += ` 
+    <tr>
+    <td>${i + 1}</td>
+    <td>${boxArray[i].name}</td>
+    <td>${boxArray[i].number}</td>
+    <td><button onclick="deleteOne(${i})">حذف</button></td>
+</tr>
+    `;
+    let tbody2: any = document.getElementById("tbody2");
+    tbody2.innerHTML = table;
+  }
+}
+function deleteOne(i) {
+  if (boxArray.length === 0) {
+    let tbody2: any = document.getElementById("tbody2");
+    tbody2.innerHTML = "";
+  } else {
+    boxArray.splice(i, 1);
+    localStorage.setItem("Needed", JSON.stringify(boxArray));
+    readingDate();
+  }
+}
+
+function Deleteall() {
+  if (boxArray.length > 0) {
+  }
+}
+readingDate();
